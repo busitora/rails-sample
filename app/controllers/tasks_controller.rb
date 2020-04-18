589a9be880b379d109a212df7426847fad8e9d85
sakamoto_t <sakamoto_t@UUNP00574.local>
2020-04-19 07:48:27 +0900

    [add]new/editの大枠

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,30 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task.create(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      flash.now[:danger] = "タスクの登録に失敗しました。"
+      render :new
+    end
+
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+    # TODO: パラメーターは後で追加する
+  end
 end
