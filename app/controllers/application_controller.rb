b25eb51fe8d6a0550cee3af5754c8d1466c65f9c
sakamoto_t <sakamoto_t@UUNP00574.local>
2020-04-27 14:57:34 +0900

    [add]ログイン機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :login_required
+  helper_method :current_user
   add_flash_types :success, :info, :warning, :danger, :notice
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to login_path, danger: 'ログインして下さい' unless current_user
+  end
 end
