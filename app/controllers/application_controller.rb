8f37a198029cc3f9c61c799ba4f518f60c2fddcb
sakamoto_t <sakamoto_t@UUNP00574.local>
2020-05-07 17:51:05 +0900

    [add]errorページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,25 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger, :notice
 
   unless Rails.env.development?
-    rescue_from Exception,                      with: :render_500
-    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+    rescue_from Exception, with: :render_500
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
   end
 
   def routing_error
-    # raise ActionController::RoutingError, params[:path]
+    raise ActionController::RoutingError, params[:path]
+    # routing error が起きたら、render_404に飛ばす
+  end
+
+
+  def render_404
+    render 'error/404', status: 404, layout: false, content_type: 'text/html'
   end
 
   private
 
-  def render_404
-    render 'error/404', status: :not_found, layout: false, content_type: 'text/html'
-  end
-
   def render_500
-    render 'error/500', status: :internal_server_error, layout: false, content_type: 'text/html'
+    render 'error/500', status: 500, layout: false, content_type: 'text/html'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # 例外
-  # get '*not_found', to: 'application#routing_error'
-  # post '*not_found', to: 'application#routing_error'
+  get '*not_found', to: 'application#routing_error'
+  post '*not_found', to: 'application#routing_error'
+  # match '*path', to: 'application#render_404', via: :all if Rails.env.development?
+
   namespace :admin do
     resources :users
   end
