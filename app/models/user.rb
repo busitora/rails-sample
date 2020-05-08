2fbd7eb96f85f1a7cdb2156237894a28e93bd803
sakamoto_t <sakamoto_t@UUNP00574.local>
2020-05-08 11:09:12 +0900

    [add]gemの追加と、管理者が一人の時に削除できない処理を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
+  before_destroy :least_one_admin
   has_secure_password
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: {case_sensitive: true}
   validates :email, presence: true, uniqueness: true
   enum role: %i[normal admin]
+
+  private
+
+  def least_one_admin
+    throw(:abort) if User.admin.count == 1
+    # TODO: 下記のようなメッセージ出したいが出せてない、毎回countするのもいかがなものか。
+    # flash.now[:danger] = "管理者が最後の一人の場合は削除できません"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # 例外
-  get '*not_found', to: 'application#routing_error'
-  post '*not_found', to: 'application#routing_error'
+  # get '*not_found', to: 'application#routing_error'
+  # post '*not_found', to: 'application#routing_error'
   # match '*path', to: 'application#render_404', via: :all if Rails.env.development?
 
   namespace :admin do
